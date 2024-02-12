b90a4b3af29f3c7a80cb8bf898206278e7adbad3
sudo-selfdestruction <eaa040104@gmail.com>
2024-02-12 20:08:12 +0300

    initial

---
+++ src/main/java/by/bsu/rfict/AnyTopicPoll/AnyTopicPollApplication.java
@@ -0,0 +1,13 @@
+package by.bsu.rfict.AnyTopicPoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyTopicPollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyTopicPollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsu/rfict/AnyTopicPoll/AnyTopicPollApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.rfict.AnyTopicPoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyTopicPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
