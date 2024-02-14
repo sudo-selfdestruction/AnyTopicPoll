80cb87d219e3a54aba0bdea0f89e5fc22bc31f7f
sudo-selfdestruction <eaa040104@gmail.com>
2024-02-14 20:14:19 +0300

    initial

---
+++ src/main/java/by/bsu/rfict/AnyTopicPoll/controller/MainController.java
@@ -0,0 +1,14 @@
+package by.bsu.rfict.AnyTopicPoll.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class MainController {
+    @GetMapping("/")
+    public String getMainPage() {
+        return "MAIN";
+    }
+}
