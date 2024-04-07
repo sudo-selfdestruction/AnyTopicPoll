859badee362cc8c2abffadb7e87d742c96c85462
sudo-selfdestruction <eaa040104@gmail.com>
2024-04-07 13:43:51 +0300

    upd: user repo

--- src/main/java/by/bsu/rfict/AnyTopicPoll/repository/UserRepository.java
+++ src/main/java/by/bsu/rfict/AnyTopicPoll/repository/UserRepository.java
@@ -2,8 +2,12 @@ package by.bsu.rfict.AnyTopicPoll.repository;
 
 import by.bsu.rfict.AnyTopicPoll.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByUsername(String email);
+    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE username = :login")
+    User getUserByLogin(String login);
 }
